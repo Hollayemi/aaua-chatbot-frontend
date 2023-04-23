4066f1bade920211769e824b0c02831d954e2c8b
Stephen Olayemi <stephenyemmitty@gmail.com>
2023-04-23 17:39:06 +0100

    added admin dashboard

--- src/state/slices/api/baseApi.js
+++ src/state/slices/api/baseApi.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
-// https://shuttle-backend.onrender.com
+// https://aaua-chatbot.onrender.com
 // 'http://localhost:2020/api/v1/ictac-bot',
 export default axios.create({
-    baseURL: 'https://shuttle-backend.onrender.com/api/v1/ictac-bot',
+    baseURL: 'https://aaua-chatbot.onrender.com/api/v1/ictac-bot',
 });
