d9db76b1c720bd0453085eb6a0c7b8aeb21a0335
Stephen Olayemi <stephenyemmitty@gmail.com>
2023-04-23 14:56:24 +0100

    edited base api

--- src/state/slices/api/baseApi.js
+++ src/state/slices/api/baseApi.js
@@ -2,5 +2,5 @@ import axios from 'axios';
 // https://shuttle-backend.onrender.com
 // 'http://localhost:2020/api/v1/ictac-bot',
 export default axios.create({
-    baseURL: 'http://localhost:2020/api/v1/ictac-bot',
+    baseURL: 'https://shuttle-backend.onrender.com/api/v1/ictac-bot',
 });
